ccc927c7b6fc39e9526061614b5d3a0be8d83c5e
Travis Scanlon <travis.scanlon@genesys.com>
2020-04-21 15:47:56 -0400

    frontend

--- challenge/challenge/urls.py
+++ challenge/challenge/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # path('', include('frontend.urls')),
+    path('', include('frontend.urls')),
     path('', include('cipher.urls')),
 ]
